da6204d541f4a8d94ace02ecc9aaa126031937b8
Arnab Goswami <79049651+arnab64g@users.noreply.github.com>
2022-12-20 13:20:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,71 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2011, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the Willow Garage, Inc. nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import cv2
-import numpy
+import rospy
+from geometry_msgs.msg import Twist
+
+import sys, select, os
+if os.name == 'nt':
+  import msvcrt, time
+else:
+  import tty, termios
+
+BURGER_MAX_LIN_VEL = 0.22
+BURGER_MAX_ANG_VEL = 2.84
+
+WAFFLE_MAX_LIN_VEL = 0.26
+WAFFLE_MAX_ANG_VEL = 1.82
+
+LIN_VEL_STEP_SIZE = 0.01
+ANG_VEL_STEP_SIZE = 0.1
+
+msg = """
+Control Your TurtleBot3!
+---------------------------
+Moving around:
+        w
+   a    s    d
+        x
+
+w/x : increase/decrease linear velocity (Burger : ~ 0.22, Waffle and Waffle Pi : ~ 0.26)
+a/d : increase/decrease angular velocity (Burger : ~ 2.84, Waffle and Waffle Pi : ~ 1.82)
+
+space key, s : force stop
+
+CTRL-C to quit
+"""
+
+e = """
+Communications Failed
+"""
+
 
 ARUCO_DICT = {
     "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
@@ -26,23 +92,9 @@ ARUCO_DICT = {
 }
 
 
-def pose_estimation(frame, aruco_dict_type, matrix_coefficients, distortion_coefficients):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.aruco_dict = cv2.aruco.Dictionary_get(aruco_dict_type)
-    parameters = cv2.aruco.DetectorParameters_create()
-    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(gray, cv2.aruco_dict, parameters=parameters,
-                                                                cameraMatrix = matrix_coefficients,
-                                                                distCoeff=distortion_coefficients)
-    if len(corners) > 0:
-        for i in range(0, len(ids)):
-            rec, tec, marker_points = cv2.aruco.estimatePoseSingleMarkers(corners[i], 0.02, matrix_coefficients,
-                                                                          distortion_coefficients)
-            cv2.aruco.drawDetectedMarkers(frame, corners)
-            cv2.aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rec, tec, 0.01)
-    return frame
-
-
-def aruco_display(corners, ids, image):
+def aruco_display(corners, ids, image, w):
+    c_x = 0
+    c_y = 0
     if len(corners) > 0:
         ids = ids.flatten()
         for (markerCorner, markerID) in zip(corners, ids):
@@ -61,102 +113,181 @@ def aruco_display(corners, ids, image):
             cv2.circle(image, (c_x, c_y), 4, (0, 0, 255), -1)
             cv2.putText(image, str(markerID), (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                         (0, 255, 0), 2)
+            cv2.putText(image, str(w), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        (0, 255, 0), 2)
             print("[Inference] ArUco marker ID: {}".format(markerID))
-    return image
-
-
-def generate_marker():
-    x, y = input('Type: ').split(' ')
-    aruco_type = 'DICT_'+x+'X'+x+'_'+y
-    m_id = int(input('Marker ID: '))
-    tag_size = int(input('Tag Size: '))
-    tag = numpy.zeros((tag_size, tag_size, 1), dtype='uint8')
-    aruco_dict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
-    cv2.aruco.drawMarker(aruco_dict, m_id, tag_size, tag, 1)
-    tag_name = input('Tag Name: ')
-    dr = 'ArUcoMarker/' + tag_name + '.png'
-    cv2.imwrite(dr, tag)
-    print('Generated')
-    image_read(dr)
-
-
-def image_read(file_name):
-    img = cv2.imread(file_name)
-    aruco_type = "DICT_5X5_1000"
-    aruco_dict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
-    aruco_params = cv2.aruco.DetectorParameters_create()
-    corners, ids, rejected = cv2.aruco.detectMarkers(img, aruco_dict, parameters=aruco_params)
-    detected_markers = aruco_display(corners, ids, img)
-    cv2.imshow('Image', detected_markers)
-    cv2.waitKey(0)
-    cv2.distroyAllWindows()
+    return image, c_x, c_y
 
 
 def webcam_read():
-    aruco_type = "DICT_5X5_100"
+    aruco_type = "DICT_4X4_100"
     aruco_dict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
     aruco_params = cv2.aruco.DetectorParameters_create()
-    vid = cv2.VideoCapture(0)
+    vid = cv2.VideoCapture('http://192.168.1.107:8080/video')
     while vid.isOpened():
         ret, img = vid.read()
         h, w, d = img.shape
         width = 1000
         height = int(width*(h/w))
-        img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
+        #img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
         corners, ids, rejected = cv2.aruco.detectMarkers(img, aruco_dict, parameters=aruco_params)
-        detected_markers = aruco_display(corners, ids, img)
+        detected_markers = aruco_display(corners, ids, img, w)
         cv2.imshow('WebCam', detected_markers)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
     vid.release()
     cv2.distroyAllWindows()
 
 
-def video_read():
-    vid = cv2.VideoCapture('testVideo.mp4')
-    while vid.isOpened():
-        ret, frame = vid.read()
-        if ret > 0:
-            cv2.imshow('Frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-    vid.release()
-    cv2.releaseAllWindows()
+def getKey():
+    if os.name == 'nt':
+        timeout = 0.1
+        startTime = time.time()
+        while(1):
+            if msvcrt.kbhit():
+                if sys.version_info[0] >= 3:
+                    return msvcrt.getch().decode()
+                else:
+                    return msvcrt.getch()
+            elif time.time() - startTime > timeout:
+                return ''
 
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
 
-def position_estimation():
-    aruco_type = "DICT_5X5_100"
-    arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
-    arucoParams = cv2.aruco.DetectorParameters_create()
-    intrinsic_camera = numpy.array(((933.15867, 0, 657.59), (0, 933.1586, 400.36993), (0, 0, 1)))
-    distortion = numpy.array((-0.43948, 0.18514, 0, 0))
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
 
-    cap = cv2.VideoCapture(0)
-    while cap.isOpened():
-        ret, img = cap.read()
-        output = pose_estimation(img, ARUCO_DICT[aruco_type], intrinsic_camera, distortion)
-        cv2.imshow('Estimated Pose', output)
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+def vels(target_linear_vel, target_angular_vel):
+    return "currently:\tlinear vel %s\t angular vel %s " % (target_linear_vel,target_angular_vel)
 
+def makeSimpleProfile(output, input, slop):
+    if input > output:
+        output = min( input, output + slop )
+    elif input < output:
+        output = max( input, output - slop )
+    else:
+        output = input
 
-n = int(input('Enter: '))
-if n == 1:
-    print('Image')
-    image_read('test.jpg')
-elif n == 2:
-    print('WebCam')
-    webcam_read()
-elif n == 3:
-    print('Videos')
-    video_read()
-elif n == 4:
-    print('Position Estimation')
-    position_estimation()
-else:
-    print('Generate Aruco')
-    generate_marker()
+    return output
 
+def constrain(input, low, high):
+    if input < low:
+      input = low
+    elif input > high:
+      input = high
+    else:
+      input = input
+
+    return input
+
+def checkLinearLimitVelocity(vel):
+    if turtlebot3_model == "burger":
+      vel = constrain(vel, -BURGER_MAX_LIN_VEL, BURGER_MAX_LIN_VEL)
+    elif turtlebot3_model == "waffle" or turtlebot3_model == "waffle_pi":
+      vel = constrain(vel, -WAFFLE_MAX_LIN_VEL, WAFFLE_MAX_LIN_VEL)
+    else:
+      vel = constrain(vel, -BURGER_MAX_LIN_VEL, BURGER_MAX_LIN_VEL)
+
+    return vel
+
+def checkAngularLimitVelocity(vel):
+    if turtlebot3_model == "burger":
+      vel = constrain(vel, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
+    elif turtlebot3_model == "waffle" or turtlebot3_model == "waffle_pi":
+      vel = constrain(vel, -WAFFLE_MAX_ANG_VEL, WAFFLE_MAX_ANG_VEL)
+    else:
+      vel = constrain(vel, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
+
+    return vel
+
+if __name__=="__main__":
+    if os.name != 'nt':
+        settings = termios.tcgetattr(sys.stdin)
+
+    rospy.init_node('turtlebot3_teleop')
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+    turtlebot3_model = rospy.get_param("model", "burger")
+
+    status = 0
+    target_linear_vel   = 0.0
+    target_angular_vel  = 0.0
+    control_linear_vel  = 0.0
+    control_angular_vel = 0.0
+
+    try:
+        print(msg)
+        aruco_type = "DICT_4X4_100"
+        aruco_dict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
+        aruco_params = cv2.aruco.DetectorParameters_create()
+        vid = cv2.VideoCapture('http://192.168.1.107:8080/video')
+        while not rospy.is_shutdown():
+            ret, img = vid.read()
+            h, w, d = img.shape
+            width = 1000
+            height = int(width*(h/w))
+            #img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
+            corners, ids, rejected = cv2.aruco.detectMarkers(img, aruco_dict, parameters=aruco_params)
+            detected_markers, x, y = aruco_display(corners, ids, img, w)
+            cv2.imshow('WebCam', detected_markers)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+              break
+            key = '0'
+            if x > 320:
+                key = 'w'
+            if key == 'w' :
+                target_linear_vel = checkLinearLimitVelocity(target_linear_vel + LIN_VEL_STEP_SIZE)
+                status = status + 1
+                print(vels(target_linear_vel,target_angular_vel))
+            elif key == 'x' :
+                target_linear_vel = checkLinearLimitVelocity(target_linear_vel - LIN_VEL_STEP_SIZE)
+                status = status + 1
+                print(vels(target_linear_vel,target_angular_vel))
+            elif key == 'a' :
+                target_angular_vel = checkAngularLimitVelocity(target_angular_vel + ANG_VEL_STEP_SIZE)
+                status = status + 1
+                print(vels(target_linear_vel,target_angular_vel))
+            elif key == 'd' :
+                target_angular_vel = checkAngularLimitVelocity(target_angular_vel - ANG_VEL_STEP_SIZE)
+                status = status + 1
+                print(vels(target_linear_vel,target_angular_vel))
+            elif key == ' ' or key == 's' :
+                target_linear_vel   = 0.0
+                control_linear_vel  = 0.0
+                target_angular_vel  = 0.0
+                control_angular_vel = 0.0
+                print(vels(target_linear_vel, target_angular_vel))
+            else:
+                if (key == '\x03'):
+                    break
+
+            if status == 20 :
+                print(msg)
+                status = 0
+
+            twist = Twist()
+
+            control_linear_vel = makeSimpleProfile(control_linear_vel, target_linear_vel, (LIN_VEL_STEP_SIZE/2.0))
+            twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
+
+            control_angular_vel = makeSimpleProfile(control_angular_vel, target_angular_vel, (ANG_VEL_STEP_SIZE/2.0))
+            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
+
+            pub.publish(twist)
+
+    except:
+        print(e)
+
+    finally:
+        twist = Twist()
+        twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+        pub.publish(twist)
+
+    if os.name != 'nt':
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
